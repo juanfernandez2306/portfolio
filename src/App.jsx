d8d966ef2e68d35b43c5f879bf5c9a5221b112c5
juanfernandez2306 <juanfernandez2306@gmail.com>
2023-03-09 12:47:51 -0400

    add style css hamburg menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import './assets/css/Hamburg_menu.css';
+import './assets/css/App.css';
 
 function App() {
 
   return (
     <>
-      hola mundo
+      <div className="container_header">
+        <header>
+          <aside>
+            <h1>jfcoordenadas</h1>
+          </aside>
+          <ul className="menu_link">
+            <li><a href="#">Acerca de mi</a></li>
+            <li><a href="#">Portafolio</a></li>
+            <li><a href="#">Escríbeme</a></li>
+          </ul>
+          <aside className="container_hamburg_menu">
+            <a href="#" className="hamburg_menu"></a>
+          </aside>
+        </header>
+      </div>
     </>
   )
 }
