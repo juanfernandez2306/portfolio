4d729a91f1b2108c3a73aa962ec42fdf5fd86f1e
juanfernandez2306 <juanfernandez2306@gmail.com>
2023-03-15 06:57:46 -0400

    add style hamburg and add plugin vite svg

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,18 +1,29 @@
+import { useRef, useState } from "react";
 function Header(){
+
+    const menuRef = useRef();
+    const [menuStatus, setMenuStatus] = useState(false);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setMenuStatus(!menuStatus);
+        
+    };
+
     return(
         <>
         <header className="header_principal">
             <aside className="logo">
                 <h1>jfcoordenadas.xyz</h1>
             </aside>
-            <ul className="links">
-                <li><a href="#">Inicio</a></li>
+            <ul className={"link_menu" + (menuStatus ? " active": "")}>
+                <li><a href="/">Inicio</a></li>
                 <li><a href="#">Proyectos</a></li>
                 <li><a href="#">Acerca de mí</a></li>
                 <li><a href="#">Escríbeme</a></li>
             </ul>
-            <aside className="container_hamburg_menu">
-                <a href="#" className="hamburg_menu"></a>
+            <aside className={"container_hamburg_menu" + (menuStatus ? " active": "")}>
+                <a href="#" ref={menuRef} onClick={handleClick} className="hamburg_menu"></a>
             </aside>
         </header>
         </>
