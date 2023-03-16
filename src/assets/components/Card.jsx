8e884ec04f455a5242ccc2f80a46d9e7e2cea341
juanfernandez2306 <juanfernandez2306@gmail.com>
2023-03-16 12:44:11 -0400

    add style componet home project and header

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -4,9 +4,9 @@ import Eyes from '../icons/Eyes'
 export default function CardProject({url_img, title, url_demo, url_code, title_btn}){
     return(
         <article className="card_project">
-            <aside className="container_img">
+            
                 <img src={url_img} alt="" />
-            </aside>
+            
             <h3>{title}</h3>
             <aside className="container_btn">
                 <a href={url_demo} target="_blank" className='btn_eyes'>
--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -2,7 +2,19 @@ import CardProject from "./Card";
 
 export default function Projects(){
     return(
-        <section className="projects">
+        <section className="container_card_project">
+            <CardProject 
+                title="localiza tu establecimiento de salud" 
+                url_img="/overview/cercania_centro_salud_2.gif"
+                url_demo="https://jfcoordenadas.xyz/cercania_centro_salud/"
+                url_code="https://github.com/juanfernandez2306/cercania_centro_salud"
+            />
+            <CardProject 
+                title="localiza tu establecimiento de salud" 
+                url_img="/overview/cercania_centro_salud_2.gif"
+                url_demo="https://jfcoordenadas.xyz/cercania_centro_salud/"
+                url_code="https://github.com/juanfernandez2306/cercania_centro_salud"
+            />
             <CardProject 
                 title="localiza tu establecimiento de salud" 
                 url_img="/overview/cercania_centro_salud_2.gif"
