cbc5203552f15960692d7636d26297d5e6655d0d
juanfernandez2306 <juanfernandez2306@gmail.com>
2026-07-18 15:51:57 -0400

    modificando el portafolio

--- src/assets/components/utils/Title.jsx
+++
@@ -1,9 +0,0 @@
-import '../../css/Title.css';
-
-export default function Title({text_title, name_id}){
-    return(
-        <section className="title" id={name_id}>
-            <h2>{text_title}</h2>
-        </section>
-    );
-}
